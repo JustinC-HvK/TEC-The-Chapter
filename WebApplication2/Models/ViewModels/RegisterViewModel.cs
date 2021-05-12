200eb225af1f8aec9dad6f540698e03a16e6320d
Xavier Joyce Davies <rockin256@gmail.com>
2021-05-12 12:53:48 +1200

    Register Page template

--- WebApplication2/Models/ViewModels/RegisterViewModel.cs
+++ WebApplication2/Models/ViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,25 @@ namespace WebApplication2.Models.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName{ get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string ReenterPassword { get; set; }
+
     }
 }
